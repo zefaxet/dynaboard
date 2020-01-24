930625ee3635df5271983640d4185389ef321d56
zephyr101101 <32856067+zephyr101101@users.noreply.github.com>
2020-01-24 09:24:07 -0600

    Python that works with button
    Created this program after relearning how to interact with the Pi

---
+++ button.py
@@ -0,0 +1,28 @@
+#buttoninput.py
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+sleeptime = .1
+
+#GPIO Pin of the component
+lightPin = 4
+buttonPin = 17
+
+GPIO.setup(lightPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.output(lightPin, False)
+
+try:
+    while True:
+        GPIO.output(lightPin, not GPIO.input(buttonPin))
+        sleep(.1)
+finally:
+    GPIO.output(lightPin, False)
+    GPIO.cleanup()
+
+GPIO.cleanup()
+
+
