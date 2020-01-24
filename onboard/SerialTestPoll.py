43aa91b3d9aa8fa6503f964ba61174060c680754
btdoughty <platypirfun@gmail.com>
2020-01-23 23:23:08 -0600

    added comments on serial comm programs

--- onboard/SerialTestPoll.py
+++ onboard/SerialTestPoll.py
@@ -1,7 +1,12 @@
 import serial
 
+# follow instructions here: https://learn.adafruit.com/turning-your-raspberry-pi-zero-into-a-usb-gadget/serial-gadget
+# in order to set up Pi as serial device
+
+# instantiates the serial port on the pi
 serialport = serial.Serial("/dev/ttyGS0", 9600, timeout=0.5)
 
+# listens on the serial port for incoming commands and prints
 while True:    
     command = serialport.read()
     print str(command)
--- onboard/SerialTestSend.py
+++ onboard/SerialTestSend.py
@@ -1,5 +1,9 @@
 import serial
 
+# instantiates the serial port on the host pc
+# on Windows, port name is listed under device manager - Ports>PiUSB to Serial
 ser = serial.Serial("COM3", 9600)
+
+# sends command over serial port
 command = b'\x41\x42\x43\x44\x48\x65\x6c\x6c\x6f'
 ser.write(command)
